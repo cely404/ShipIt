f081e32b76f1f6474e5a034f76bd4fe271cd4af4
cely404 <aracely_payan93@yahoo.com>
2014-08-01 13:02:34 -0700

    Most of melvin's menu ready

--- shipItPOC/gen/mitchell/ShipIt/R.java
+++ shipItPOC/gen/mitchell/ShipIt/R.java
@@ -915,9 +915,15 @@ containing a value of this type.
         public static final int abs__textfield_searchview_right_holo_light=0x7f02006e;
         public static final int abs__toast_frame=0x7f02006f;
         public static final int buttona=0x7f020070;
-        public static final int ic_action_search=0x7f020071;
-        public static final int ic_launcher=0x7f020072;
-        public static final int list_icon=0x7f020073;
+        public static final int friday=0x7f020071;
+        public static final int ic_action_search=0x7f020072;
+        public static final int ic_launcher=0x7f020073;
+        public static final int list_icon=0x7f020074;
+        public static final int local=0x7f020075;
+        public static final int monday=0x7f020076;
+        public static final int thursday=0x7f020077;
+        public static final int tuesday=0x7f020078;
+        public static final int wednesday=0x7f020079;
     }
     public static final class id {
         public static final int abs__action_bar=0x7f040020;
