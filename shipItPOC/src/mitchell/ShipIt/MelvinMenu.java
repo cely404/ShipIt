069e8bc9e48d2854c6c2ed930451d85b4218c99b
cely404 <aracely_payan93@yahoo.com>
2014-08-01 13:22:53 -0700

    melvins menu done-ish

--- shipItPOC/src/mitchell/ShipIt/MelvinMenu.java
+++ shipItPOC/src/mitchell/ShipIt/MelvinMenu.java
@@ -1,10 +1,14 @@
 
 package mitchell.ShipIt;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -16,7 +20,9 @@ import com.googlecode.androidannotations.annotations.EActivity;
 
 public class MelvinMenu extends SherlockActivity
 {
-Menu mymenu;
+	Button monday, tuesday, wednesday, thursday, friday, local;
+	TextView mytext;
+	Menu mymenu;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,6 +33,60 @@ Menu mymenu;
 		setTitle("The Melvin Menu");
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		monday = (Button) findViewById(R.id.button1);
+		tuesday = (Button) findViewById(R.id.button2);
+		wednesday = (Button) findViewById(R.id.button3);
+		thursday = (Button) findViewById(R.id.button4);
+		friday = (Button) findViewById(R.id.button5);
+		local = (Button) findViewById(R.id.button6);
+		mytext = (TextView) findViewById(R.id.editText1);
+		
+        monday.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	mytext.setText("its monday");
+            	
+            }
+        });
+        tuesday.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	mytext.setText("its tuesday");
+
+            }
+        });
+        wednesday.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	mytext.setText("its wednesday");
+
+            }
+        });
+        thursday.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	mytext.setText("its thursday");
+
+            }
+        });
+        friday.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	mytext.setText("its friday");
+
+            }
+        });
+        local.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+				//Intent i;
+				//i=new Intent(this, LocalGrubActivity.class);
+				//startActivity(i);
+            	mytext.setText("I will display local restaurants");
+
+            }
+        });
+
 
 	}
     @Override
